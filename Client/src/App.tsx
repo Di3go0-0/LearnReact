890c506883434b3d704346fb2cd771c450ddf5fc
di3go0-0 <d.rincon@utp.edu.co>
2024-10-30 21:56:20 -0500

    Class UseEffect

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,43 +1,76 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Button } from "./Components/";
+
+//Unidad logica
+// 1- States example: {const[data, setData] = useState([])}
+//
+// 2- Methods : const fetchData = () => {}
+//
+// 3- Effect
 
 function App() {
-  const [Count, setCount] = useState(0);
-  const [Name, setName] = useState("Diego");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  // batching
-  // const IncreaseLocalCount = () => {
-  //   setCount(Count + 1); // 0 + 1
-  //   setCount(Count + 1); // 0 + 1
-  //   setCount(Count + 1); // 0 + 1
-  //   setCount(Count + 1); // 0 + 1
-  //   setCount(Count + 1); // 0 + 1
-  // };
-
-  const IncreaseLocalCount = () => {
-    setCount((Count) => Count + 1); // (0) => 0 + 1
-    setCount((Count) => Count + 1); // (1) => 1 + 1
-    setCount((Count) => Count + 1); // (2) => 2 + 1
-    setCount((Count) => Count + 1); // (3) => 3 + 1
-    setCount((Count) => Count + 1); // (4) => 4 + 1
+  const consoleLoader = (loagingValue: boolean) => {
+    setLoading(loagingValue);
+    console.log("Loading: ", loading);
   };
 
-  const changeName = () => {
-    if (Name === "Juan") {
-      setName("Diego");
-      return;
+  const fetchData = async () => {
+    consoleLoader(true);
+    try {
+      const response = await fetch("https://api.example.com/data");
+      if (!response.ok) {
+        throw new Error("Error fetching data");
+      }
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (e) {
+      setError(e as string);
+    } finally {
+      consoleLoader(false);
     }
-    setName("Juan");
   };
 
-  return (
-    <>
-      <Button label={`Count is ${Count}`} parentMethod={IncreaseLocalCount} />
-      <p>{Name}</p>
-      <Button label={"Change Name"} parentMethod={changeName} />
-    </>
-  );
-}
+  //  Normalmente pondriamos en arreglo de dependencias {data}, aunque implicitamente se maneja
+  //  el data con el fetchData, no es necesario pornerlo en el arreglo de dependecias del useEffect
+  //  porque no lo usa directamente, y si lo ponemos este crearia un bucle.
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  return <div>{JSON.stringify(data)}</div>;
+}
 export default App;
+
+// Decl
+////Mal uso Del UseEffect
+//  //Maneja el estado de la aplicacion
+//  useEffect(
+//    () => {
+//      //Logica
+//      // 1- cuando se monta el componente
+//      // 2- cada vez que se modificque uno de los valores del segundo parametro
+//      //
+//      return () => {
+//        // Manejar el estado de la memoria
+//      };
+//    },
+//    [
+//      /* {`Arreglo de dependencias`} state */
+//    ],
+//  );
+//  //Correcto us el useEffect
+//  // Comunicamos con un API
+//  // sincronizar con entidades externas
+//  // operacioes asynconas
+//  // parametros de entrada
+//
